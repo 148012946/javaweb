25655a78c5204f03e50a2b69bc9c66da89c2219f
winFzj <148012946@qq.com>
2020-11-16 20:44:47 +0800

    过滤器demo，模拟登录

---
+++ src/com/mashibing/servlet/LoginServlet.java
@@ -0,0 +1,23 @@
+package com.mashibing.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Administrator
+ */
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request,response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("loginservlet的处理过程");
+        response.getWriter().write("登陆成功");
+    }
+}
